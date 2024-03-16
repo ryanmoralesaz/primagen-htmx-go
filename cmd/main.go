6dba90d2be18b2141db0e00a209b4bb431e86d75
Ryan Morales <ryanmoralesaz@users.noreply.github.com>
2024-03-16 20:41:28 +0000

    got to minute 24 button not working

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"io"
+	"text/template"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Templates struct {
+	template *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.template.ExecuteTemplate(w, name, data)
+}
+func newTemplate() *Templates {
+	return &Templates{
+		template: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
+	count := Count{Count: 0}
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index.html", count)
+	})
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index.html", count)
+	})
+	e.Logger.Fatal(e.Start(":4200"))
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,9 @@ module mywebsite.tv/name
 
 go 1.22.1
 
-require (
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/echo/v4 v4.11.4
-)
+require github.com/labstack/echo/v4 v4.11.4
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
