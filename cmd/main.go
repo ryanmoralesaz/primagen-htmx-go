3af3691baec686542b3484955c4914b7ecf98ac2
Ryan Morales <ryanmoralesaz@users.noreply.github.com>
2024-03-17 03:59:16 +0000

    stuck at 1:13 500 error when serving to /contacts post

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"io"
-	"log"
-	"os"
 	"text/template"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
-	template *template.Template
+	templates *template.Template
 }
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.template.ExecuteTemplate(w, name, data)
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func newTemplate() *Templates {
 	return &Templates{
-		template: template.Must(template.ParseGlob("views/*.html")),
+		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
 }
 
@@ -41,13 +40,45 @@ type Data struct {
 	Contacts Contacts
 }
 
+func (d *Data) hasEmail(email string) bool {
+	for _, c := range d.Contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
+}
 func newData() Data {
 	return Data{
 		Contacts: []Contact{
-			newContact("John Doe", "jd@gmail.com"),
+			newContact("aoeu", "jd@gmail.com"),
 			newContact("Jane Doe", "cd@gmail.com"),
 		},
 	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
 }
 
 type Count struct {
@@ -55,25 +86,32 @@ type Count struct {
 }
 
 func main() {
-	wd, _ := os.Getwd()
-	log.Println("Current working directory:", wd)
-
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	data := newData()
+	page := newPage()
+
+	// data := newData()
 
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", data)
+		return c.Render(200, "index", page)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
-		data.Contacts = append(data.Contacts, newContact(name, email))
-		return c.Render(200, "index", data)
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+			return c.Render(422, "form", formData)
+		}
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+		return c.Render(200, "display", page)
 	})
 
 	e.Logger.Fatal(e.Start(":4200"))
